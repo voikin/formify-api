979623839d64c60f5c3571fd745789f471fbd7df
Nikita Panferov <nikpanfer@gmail.com>
2024-03-17 21:54:38 +0300

    add alembic and fix pydantic config class

--- configs/config.py
+++ configs/config.py
@@ -2,25 +2,34 @@ from misc.singleton import singleton
 from pathlib import Path
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
-from threading import Lock
 
 BASE_DIR = Path(__file__).parent.parent
 
 
-class DBSettings(BaseModel):
+class DBSettings(BaseSettings):
     url: str = "postgresql+asyncpg://postgres:changeme@localhost:5432/"
     echo: bool = False
 
+    class Config:
+        env_file = ".env"
+        env_prefix = "DB_"
+        extra = "ignore"
 
-class AuthJWT(BaseModel):
+
+class AuthJWT(BaseSettings):
     private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
     public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
     algorithm: str = "RS256"
     access_token_expire_minutes: int = 30
     refresh_token_expire_minutes: int = 60
 
+    class Config:
+        env_file = ".env"
+        env_prefix = "JWT_"
+        extra = "ignore"
+
 
 @singleton
-class Settings(BaseSettings):
+class Settings(BaseModel):
     jwt: AuthJWT = AuthJWT()
     db: DBSettings = DBSettings()
